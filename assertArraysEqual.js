e1df1b7042012d603f1513b58807b3811414b370
priyaprecious <priyanka.datti@gmail.com>
2021-10-29 03:36:55 +0000

    modified assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+
+
+  const assertArraysEqual = function(setA, setB) {
+    for (let i = 0; i < setA.length && setB.length; i++) {
+      if (setA[i] !== setB[i]) {
+                                                  //[A]-actual  //[B]-expected
+        console.log(`🟥 Assertion Failed: ${setA} !== ${setB}`);
+      }
+    }                                           //[A]-actual  //[B]-expected
+    console.log(`🟩 Assertion Passed: ${setA} === ${setB}`);
+  };
+
+
+  assertArraysEqual([1, 2, 3], [1, 2, 3]);
+  assertArraysEqual([3, 2, 3], [3, 2, 3]);
+  assertArraysEqual([4, 2, 3], [1, 2, 3]);
+  assertArraysEqual([1, 2, 3], [4, 2, 3]);
